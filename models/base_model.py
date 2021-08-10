f0e5fee940bb13ad50a06897effd63ed9fe2fd6c
klich404 <klich4010@gmail.com>
2021-08-10 18:15:12 -0500

    base_mode.py updated and add imports in city.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from enum import unique
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import false
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(String(60), nullable=False, unique=True, primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -13,7 +24,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -32,6 +42,7 @@ class BaseModel:
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
+        storage.new(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -41,4 +52,11 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if "__sa_instance_state" in dictionary:
+            dictionary.pop("__sa_instance_state", 0)
         return dictionary
+
+    def delete(self):
+        """ """
+        from models import Storage
+        Storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.sql.expression import column
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = column(String(60), nullable=False)
-    state_id = column(String(60), nullable=False, foreignKey('states.id'))
+    state_id = column(String(60), ForeignKey('states_id'), nullable=False)
