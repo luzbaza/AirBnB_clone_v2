5589b401a8986702c39207877fb754cffb37e487
klich404 <klich4010@gmail.com>
2021-08-11 15:56:27 -0500

    relationship add in user and city with places

--- models/city.py
+++ models/city.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy.sql.expression import column
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -10,3 +10,5 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(60), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    places = relationship('Places', backref='cities', cascade='all, delete')
