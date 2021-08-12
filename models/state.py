a9916d95b05c8b219e9e3b5685ba758e02c4b3de
klich404 <klich4010@gmail.com>
2021-08-11 20:59:00 -0500

    sintax errors fixed

--- models/state.py
+++ models/state.py
@@ -15,7 +15,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        cities= relationship('City', backref='state', cascade='all, delete')
+        cities = relationship('City', backref='state', cascade='all, delete')
 
     else:
         @property
