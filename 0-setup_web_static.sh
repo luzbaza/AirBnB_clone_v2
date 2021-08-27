c980673cfe585823a42f325ee179b647a910c19d
luzbaza <2459@holbertonschool.com>
2021-08-26 19:18:03 -0500

    Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#script that sets up your web servers for the deployment of web_static
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i "47 a \\\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\tautoindex off;\n\t}\n" /etc/nginx/sites-available/default
+service nginx restart
