516297d2ec3c6a40956f13c9ddf547cf5e3d49fb
luzbaza <2459@holbertonschool.com>
2021-09-11 14:19:35 -0500

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """display “C ” followed by the value of the text variable"""
-    return 'C %s' + text.replace('_', ' ')
+    return 'C' + text.replace('_', ' ')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
